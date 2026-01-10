aa6cc2342f0a7854d2f5edede4200f1daf90a898
yu <yul1024@outlook.com>
2026-01-10 20:33:08 +0800

    feat: add pixi installation methods

---
+++ windows/install_pixi.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    安装pixi的方法。
+
+.LINK
+    https://github.com/yuliu625/Yu-Deployment-Toolkit/windows/intall_pixi.ps1
+
+.LINK
+    https://pixi.prefix.dev/latest/
+
+.NOTES
+    Source: https://github.com/yuliu625/Yu-Deployment-Toolkit/windows/intall_pnpm.ps1
+    Reference: https://pixi.prefix.dev/latest/
+#>
+
