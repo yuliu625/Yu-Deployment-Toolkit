890cc1f813eb411ee8affc460d1236a9c8d937da
yu <yul1024@outlook.com>
2026-04-23 16:14:59 +0800

    docs: update docstring

--- windows/install_chocolatey.ps1
+++ windows/install_chocolatey.ps1
@@ -9,8 +9,11 @@
     https://chocolatey.org/
 
 .NOTES
-    Source: https://github.com/yuliu625/Yu-Deployment-Toolkit/windows/intall_chocolatey.ps1
-    Reference: https://chocolatey.org/
+    Sources:
+        https://github.com/yuliu625/Yu-Deployment-Toolkit/windows/intall_chocolatey.ps1
+
+    References:
+        https://chocolatey.org/
 
     安装 choco 管理工具。
 #>
@@ -19,6 +22,7 @@
 # 首先检查目录，chocolatey会拒绝已有文件下安装。
 # 目录在: C:\ProgramData\chocolatey
 # 如果存在，直接删除。chocolatey会避免覆盖现有数据，但这个错误常会出现在网络导致的问题。
+
 
 # https://chocolatey.org/install#individual
 # 基于个人版本的，官方脚本直接安装。暂时给予完整权限。
