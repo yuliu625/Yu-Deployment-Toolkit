8760d8150b2570e1f8706df8913b91694c776899
yu <yul1024@outlook.com>
2026-04-23 16:18:09 +0800

    feat: add pandoc installation methods

---
+++ windows/install_pandoc.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+    安装 pandoc 的方法。
+
+.LINK
+    https://github.com/yuliu625/Yu-Deployment-Toolkit/windows/intall_pandoc.ps1
+
+.LINK
+    https://pandoc.org/
+
+.NOTES
+    Sources:
+        https://github.com/yuliu625/Yu-Deployment-Toolkit/windows/intall_pandoc.ps1
+
+    References:
+        https://pandoc.org/
+#>
+
+
+
