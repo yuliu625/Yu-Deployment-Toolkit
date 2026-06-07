fc153940838913caa8b695f115175b68f7d3444b
yu <yul1024@outlook.com>
2026-06-07 18:55:01 +0800

    refactor: rename and move

---
+++ modules/windows/install_pandoc.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+    安装 pandoc 的方法。
+
+.LINK
+    https://github.com/yuliu625/Yu-Deployment-Toolkit/blob/main/modules/windows/intall_pandoc.ps1
+
+.LINK
+    https://pandoc.org/
+
+.NOTES
+    Sources:
+        https://github.com/yuliu625/Yu-Deployment-Toolkit/blob/main/modules/windows/intall_pandoc.ps1
+
+    References:
+        - https://github.com/jgm/pandoc
+        - https://pandoc.org/
+        - https://pandoc.org/installing.html
+
+    安装 pandoc 这个重要工具的方法。这个工具对于现有的混乱写作格式非常重要。
+#>
+
+
+
