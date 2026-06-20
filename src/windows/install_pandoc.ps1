b9ee58f777a9b0a70f4523fba0b5b766ec72a4b5
yu <yul1024@outlook.com>
2026-06-20 13:29:56 +0800

    refactor: rename and move

--- src/windows/install_pandoc.ps1
+++ src/windows/install_pandoc.ps1
@@ -3,14 +3,14 @@
     安装 pandoc 的方法。
 
 .LINK
-    https://github.com/yuliu625/Yu-Deployment-Toolkit/blob/main/modules/windows/intall_pandoc.ps1
+    https://github.com/yuliu625/Yu-Deployment-Toolkit/blob/main/src/windows/intall_pandoc.ps1
 
 .LINK
     https://pandoc.org/
 
 .NOTES
     Sources:
-        https://github.com/yuliu625/Yu-Deployment-Toolkit/blob/main/modules/windows/intall_pandoc.ps1
+        https://github.com/yuliu625/Yu-Deployment-Toolkit/blob/main/src/windows/intall_pandoc.ps1
 
     References:
         - https://github.com/jgm/pandoc
